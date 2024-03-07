65ffe00da256b3d6e849d2a41e0e27b24f8ef42f
aditya navale <aditya.navale.399@gmail.com>
2024-03-07 10:15:11 +0530

    js file added

---
+++ script.js
@@ -0,0 +1,22 @@
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const container = document.querySelector(".container");
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.add("sign-in-mode");
+});
+
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-up-mode");
+});
+
+//
+sign_up_btn.addEventListener('click', () =>{
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () =>{
+    container.classList.remove("sign-in-mode");
+});
+//sign in
+
